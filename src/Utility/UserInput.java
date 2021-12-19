0d8a2031f3903b34153a2b57e39d05733652fa8e
Marharyta Apanasevich <marapa@student.chalmers.se>
2021-12-19 11:45:01 +0100

    refactored menu to remove business logic, added file separators

--- src/Utility/UserInput.java
+++ src/Utility/UserInput.java
@@ -21,18 +21,12 @@ public class UserInput {
 
     public static double readDouble(String message) {
         System.out.print(message);
-        double value = 0;
-        while (value == 0) {
-            try {
-                value = Double.parseDouble(input.next());
-            } catch (Exception e) {
-                System.out.print("Invalid input. Please type digits only: ");
-            }
-        }
+        double value = input.nextDouble();
+        input.nextLine();
         return value;
     }
 
-    public static void exitScanner() {
+        public static void exitScanner() {
         input.close();
     }
 }
