be4453350ba4c250e09a0247c362d868857ec232
Marharyta Apanasevich <marapa@student.chalmers.se>
2021-12-20 22:47:28 +0100

    bug fixes

--- src/Utility/UserInput.java
+++ src/Utility/UserInput.java
@@ -1,4 +1,5 @@
 package Utility;
+
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
@@ -21,28 +22,32 @@ public class UserInput {
     }
 
     public static double readDouble(String message) {
-        System.out.print(message);
-        double value = input.nextDouble();
-        input.nextLine();
-        return value;
+        try {
+            System.out.print(message);
+            double value = input.nextDouble();
+            input.nextLine();
+            return value;
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid entry. Please only use digits.");
+            input.nextLine();
+        } return readDouble(message);
     }
 
-    public static String readLineYesNo() {
-        System.out.print("Type Yes or No: ");
-        String sentence = input.nextLine().trim().toLowerCase(Locale.ROOT);
-        if(sentence.equals("yes")) {
-            return sentence;
-        } else if(sentence.equals("no")){
-            return sentence;
-        } else {
-            System.out.println("Please write either Yes or No");
-            return "";
+        public static String readLineYesNo () {
+            System.out.print("Type Yes or No: ");
+            String sentence = input.nextLine().trim().toLowerCase(Locale.ROOT);
+            if (sentence.equals("yes")) {
+                return sentence;
+            } else if (sentence.equals("no")) {
+                return sentence;
+            } else {
+                System.out.println("Please write either Yes or No");
+                return "";
+            }
+        }
+
+        public static void exitScanner () {
+            input.close();
         }
     }
-
-
-        public static void exitScanner() {
-        input.close();
-    }
-}
 
