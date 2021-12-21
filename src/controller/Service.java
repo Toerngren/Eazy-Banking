529db73bb7c11bdd7a7379e1254d3bae9c167a5a
Adrian Niklasson <gusnikada@student.gu.se>
2021-12-21 14:45:59 +0100

    trying merge again

--- src/controller/Service.java
+++ src/controller/Service.java
@@ -844,7 +844,7 @@ public class Service {
         return message;
     }
 
-// Monthly payment = loan amount x mr(1 + mr)^b / (1 + mr)^b – 1
+    // Monthly payment = loan amount x mr(1 + mr)^b / (1 + mr)^b – 1
     public double getMonthlyPayment(Customer currentUser) {
         Loan approvedLoan = findLoan(currentUser);
         double monthlyInterestRate = 0.023 / 1200;
